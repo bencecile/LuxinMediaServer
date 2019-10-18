d53cd6d44d17c8396ccf0e3fcf4cd2c738c6c745
Benjamin Cecile <bencecile@gmail.com>
2019-10-18 00:22:39 -0400

    Can send a hello

--- src_client/main.rs
+++ src_client/main.rs
@@ -1,3 +1,15 @@
+use std::net::{Ipv4Addr};
+
+use comms_lib::{Client, ClientCommand, ServerCommand};
+
 fn main() {
-    println!("Hello world from client");
+    let mut client = Client::connect(Ipv4Addr::LOCALHOST)
+        .expect("Failed to connect the client");
+    client.send(ClientCommand::Hello)
+        .expect("Failed to send the client hello");
+    match client.recv().expect("Failed to receive the server's response") {
+        ServerCommand::Hello => {
+            println!("Got a hello from the server");
+        },
+    }
 }
--- src_server/main.rs
+++ src_server/main.rs
@@ -1,3 +1,15 @@
+use comms_lib::{ClientCommand, Server, ServerCommand};
+
 fn main() {
-    println!("Hello world from server");
+    let server = Server::start()
+        .expect("Failed to start the server");
+    let mut server_conn = server.accept()
+        .expect("Failed to accept a new connection");
+    match server_conn.recv().expect("Failed to receive on the server connection") {
+        ClientCommand::Hello => {
+            println!("Got a hello from the client");
+            server_conn.send(ServerCommand::Hello)
+                .expect("Failed to send the hello command back");
+        }
+    }
 }
